791a0af2a508d29594abd92b966db10732dce7db
Just Playin <jp@justplayingames.com>
2025-03-02 23:37:02 -0600

    fixed bugs in graph.sh

--- bin/graph.sh
+++ bin/graph.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 DOT_FILE=docs/mac_ip_port_service.dot
-echo 'diagraph mac_ip_port_service {' > $DOT_FILE
+echo 'dagraph mac_ip_port_service {' > $DOT_FILE
+echo 'rankdir=LR;';
 echo 'node [shape=box];' >> $DOT_FILE
-cat docs/mac.txt | awk '"{print $1}";' >> $DOT_FILE
+cat docs/mac.txt | awk '{printf "\"%s\"\n", $1};' >> $DOT_FILE
 echo 'node [shape=ellipse];' >> $DOT_FILE
-cat docs/ip.txt | awk '"{print $1}";' >> $DOT_FILE
+cat docs/ip.txt | awk '{printf "\"%s\"\n", $1};' >> $DOT_FILE
 echo 'node [shape=circle];' >> $DOT_FILE
-cat docs/port.txt | awk '"{print $1}";' >> $DOT_FILE
+cat docs/port.txt | awk '{printf "\"%s\"\n", $1};' >> $DOT_FILE
 echo 'node [shape=ellipse];' >> $DOT_FILE
-cat docs/service.txt | awk '"{print $1}";' >> $DOT_FILE
+cat docs/service.txt | awk '{printf "\"%s\"\n", $1};' >> $DOT_FILE
 
-cat docs/mac_ip.txt | awk '"{print $1}" -> "{print $2}";' >> $DOT_FILE
-cat docs/ip_port.txt | awk '"{print $1}" -> "{print $2}";' >> $DOT_FILE
-cat docs/port_service.txt | awk '"{print $1}" -> "{print $2}";' >> $DOT_FILE
+cat docs/mac_ip.txt | awk '{printf "\"%s\" -> \"%s\"\n", $1, $2}' >> $DOT_FILE
+cat docs/ip_port.txt | awk '{printf "\"%s\" -> \"%s\"\n", $1, $2}' >> $DOT_FILE
+cat docs/port_service.txt | awk '{printf "\"%s\" -> \"%s\"\n", $1, $2}' >> $DOT_FILE
 
-echo '}' > $DOT_FILE
+echo '}' >> $DOT_FILE
